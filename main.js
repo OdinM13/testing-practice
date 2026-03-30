8ecdd00a69e81836c1e7e3dcb0b061b87df0f371
OdinM13 <markodinius@gmail.com>
2026-03-30 22:59:05 +0200

    Build solution for more problems

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-export { capitalize };
-
-function capitalize(word) {
+export function capitalize(word) {
   const firstLetter = word.slice(0, 1);
   const firstLetterCap = firstLetter.toUpperCase();
   const wordCap = firstLetterCap + word.slice(1); 
   return wordCap;
+}
+
+export function reverseString(word) {
+  const wordArray = word.split('');
+  const reversedArray = wordArray.reverse();
+  return reversedArray.join('');
+}
+
+export const calculator = {
+  add: (a, b) => a + b, 
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => (b === 0) ? 'Dividing by zero is not possible' : (a / b)
+}
+
+export function caesarCipher (word, shift) {
+// charCodeAt()
+// fromCharCode()
+}
+
+export function analyzeArray(arr) {
+  return obj = {
+    average: arr.reduce((a, b) => a + b, 0) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length
+  }
 }
--- main.test.js
+++ main.test.js
@@ -1,6 +1,60 @@
-import { capitalize } from './main.js';
+import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from './main.js';
 
 test('capitalize Test', () => {
-  console.log(capitalize('home'));
+  console.log('Capitalize Test: ' + capitalize('home'));
   expect(capitalize('home')).toBe('Home');
 })
+
+test('reverseString Test', () => {
+  console.log('reverseString Test: ' + reverseString('Palindrom'));
+  expect(reverseString('Palindrom')).toBe('mordnilaP');
+})
+
+test('calculator add', () => {
+  console.log('calculator Add: ' + calculator.add(2, 3));
+  expect(calculator.add(2, 3)).toBe(5);
+})
+
+test('calculator subtract', () => {
+  console.log('calculator subtract: ' + calculator.subtract(6, 2));
+  expect(calculator.subtract(6, 2)).toBe(4);
+})
+
+test('calculator multiply', () => {
+  console.log('calculator multiply: ' + calculator.multiply(6, 4));
+  expect(calculator.multiply(6, 4)).toBe(24);
+})
+
+test('calculator divide', () => {
+  console.log('calculator divide: ' + calculator.divide(10, 2));
+  expect(calculator.divide(10, 2)).toBe(5);
+})
+
+test('calculator divide by zero', () => {
+  console.log('calculator divide by zero: ' + calculator.divide(7, 0));
+  expect(calculator.divide(7, 0)).toBe('Dividing by zero is not possible');
+})
+
+test('caesarCipher Test 1', () => {
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+})
+
+test('caesarCipher Test 2', () => {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+})
+
+test('caesarCipher Test 3', () => {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+})
+
+const testObject = {
+   average: 4,
+   min: 1,
+   max: 8,
+   length: 6
+};
+
+test('analyzeArray Test', () => {
+  console.log('analyzeArray Test: ', analyzeArray([1,8,3,4,2,6]));
+  expect(analyzeArray([1,8,3,4,2,6])).toMatchObject(testObject);
+});
